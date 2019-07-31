e7aa6e510d9ad5b5c72699b4ed807feb384a6085
Gerard Ryan <gryan@fexco.com>
2019-07-31 17:22:12 +0100

    Working with routes

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,12 @@ import { Component, Input} from '@angular/core';
     <div class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date: {{event?.date}}</div>
-    <div>Time: {{event?.time}}</div>
+    <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+    </div>
     <div>Price: \${{event?.price}}</div>
     <div [hidden]="!event?.location">
       <span>Location: {{event?.location?.address}}</span>
@@ -16,13 +21,35 @@ import { Component, Input} from '@angular/core';
   </div>
   `,
   styles: [`
-    .thumbnail {min-height: 210px; }
-    .pad-left { margin-left: 10px; }
-    .well div {color: #bbb; }
-  `]
+      .bold {font-weight: bold;}
+
+  .green {
+      color: #003300 !important;
+  }
+
+  .thumbnail {
+      min-height: 210px;
+  }
+
+  .pad-left {
+      margin-left: 10px;
+  }
+
+  .well div {
+      color: #bbb;
+  }    `]
 })
 export class EventThumbnailComponent {
        @Input()  event: any;
-       someProperty: any = 'some value';
+
+
+        getStartTimeStyle() {
+          if (this.event && this.event.time === '8:00 am') {
+            return {color: '#003300', 'font-weight': 'bold'};
+          } else {
+            return [];
+          }
+
+        }
 }
 
