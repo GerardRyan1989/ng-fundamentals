aabfd10ef6905f33ce8a9594aff10facaeb288dd
Gerard Ryan <gryan@fexco.com>
2019-08-01 11:51:17 +0100

    Added barrels to reduce the number of import statements in files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
+
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivatorService,
+  EventsListResolverService
+} from './events/index';
+import {  EventsAppComponent } from './events-app.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import {EventDetailsComponent} from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
-import {CreateEventComponent } from './events/create-event.component';
 import {Error404Component} from './errors/404.components';
-import {EventRouteActivatorService} from './events/event-details/event-route-activator.service';
-import {EventsListResolverService} from './events-list-resolver.service';
 
 
 @NgModule({
@@ -43,7 +46,7 @@ export class AppModule { }
 
 export function  checkDirtyState(component: CreateEventComponent) {
   if (component.isDirty) {
-    return window.confirm('You have not saved this event, do you wish to save before leaving this page?')
+    return window.confirm('You have not saved this event, do you wish to save before leaving this page?');
   } else {
     return true;
   }
--- src/app/events/events-list-resolver.service.ts
+++ src/app/events/events-list-resolver.service.ts
@@ -1,6 +1,6 @@
 import {forwardRef, Inject, Injectable} from '@angular/core';
 import { Resolve } from '@angular/router';
-import { EventService} from './events/shared/event.service';
+import { EventService} from './shared/event.service';
 import { map } from 'rxjs/operators';
 
 @Injectable()
