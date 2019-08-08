bbb23c313367356bb359e1188d1e99503314e22f
Gerard Ryan <gryan@fexco.com>
2019-08-08 09:15:36 +0100

    removed basic unit tests and added new tests for the voter service component

---
+++ src/app/events/event-details/voter.service.spec.ts
@@ -0,0 +1,43 @@
+import { VoterService} from './voter.service';
+import { ISession} from '../shared';
+import { Observable , of} from 'rxjs';
+
+describe('voterService', () => {
+  // tslint:disable-next-line:one-variable-per-declaration
+  let voterService: VoterService,
+  mockHttp;
+  beforeEach(() => {
+    mockHttp = jasmine.createSpyObj('mockHttp', ['delete', 'post'])
+    voterService = new VoterService(mockHttp);
+  });
+
+  describe('deleteVoter', () => {
+    it('should remove voter from list of voters', () => {
+      const session = {id: 6, voters: ['joe', 'john']};
+      mockHttp.delete.and.returnValue(of(false))
+      voterService.deleteVoter(3, session as ISession, 'joe');
+
+      expect(session.voters.length).toBe(1);
+      expect(session.voters[0]).toBe('john');
+    });
+
+    it('Should call http.delete with the right URL', () => {
+      const session = {id: 6, voters: ['joe', 'john']};
+      mockHttp.delete.and.returnValue(of(false))
+      voterService.deleteVoter(3, session as ISession, 'joe');
+
+      expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
+
+    });
+  });
+
+  describe('addVoter', () => {
+    it('Should call http.post with the right URL', () => {
+      const session = {id: 6, voters: ['john']};
+      mockHttp.post.and.returnValue(of(false));
+      voterService.addVoter(3, session as ISession, 'joe');
+
+      expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', {}, jasmine.any(Object));
+    });
+  });
+})
